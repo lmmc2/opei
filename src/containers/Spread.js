c189449ddcd81df47abf330b6e99509ad2c515ce
Pedro Tôrres <t0rr3sp3dr0@gmail.com>
2018-06-15 12:56:30 -0300

    Update Spread.js

--- src/containers/Spread.js
+++ src/containers/Spread.js
@@ -16,10 +16,10 @@ export default class Spread extends Component {
 							<div>
 								<div className="practice-menuContainer">
 									<p className="index-sectionText" style={{marginBottom: "40px"}}>Professores, escolas, pais e alunos: vocês também podem ser multiplicadores! Te encorajamos à imprimir o cartaz à baixo e colar em um mural na instituição de ensino em que frequenta. Isso vai ajudar outros alunos à descobrir o mundo da informática e conhecer a OPEI!</p>
-									<Button secondary href="https://drive.google.com/a/cin.ufpe.br/uc?authuser=0&id=1Y29jK8qWneB6cSV7w5fTmSnhJeuwcv-r&export=download">Baixar Cartaz!</Button>
+									<Button secondary href="https://drive.google.com/uc?id=1xDfe5g7Vhcj4jXPJY-QKRMIqgDW_X2P5&export=download">Baixar Cartaz!</Button>
 								</div>
 								<div className="index-iframe">
-									<iframe title="spread" style={{border: "none", height: "600px"}} src="https://drive.google.com/file/d/1Y29jK8qWneB6cSV7w5fTmSnhJeuwcv-r/preview" width="640" height="480"></iframe>
+									<iframe title="spread" style={{border: "none", height: "600px"}} src="https://drive.google.com/file/d/1xDfe5g7Vhcj4jXPJY-QKRMIqgDW_X2P5/preview" width="640" height="480"></iframe>
 								</div>
 							</div>
 						</div>
